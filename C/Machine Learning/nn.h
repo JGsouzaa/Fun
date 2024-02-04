f793cbeb02e7176c60972b68b006dde11447d78b
J. Souza <99425339+JGsouzaa@users.noreply.github.com>
2024-02-04 12:58:51 -0300

    Implemented matrices
    Matrices print, Matrices memory allocation, matrices random generator with seed

--- C/Machine Learning/nn.h
+++ C/Machine Learning/nn.h
@@ -1,3 +1,20 @@
+#include <stddef.h>
+#include <stdio.h>
+
+#ifndef NN_MALLOC
+#include <stdlib.h>
+#define NN_MALOC malloc
+#endif //NN_MALOC
+
+#ifndef NN_ASSERT
+#include <assert.h>
+#define NN_ASSERT assert
+#endif
+
+#define MAT_AT(m, i, j) (m).es[(i)*(m).cols + (j)] //This is a macro that takes the memory addres and manipullates to get the element by iteration i and j
+
+float rand_float(void);
+
 #ifndef NN_H_
 #define NN_H_
 
@@ -8,11 +25,56 @@ typedef struct {
 } Mat;
 
 Mat mat_alloc(size_t rows, size_t cols);
-void mat_dot();
-void mat_sum();
+void mat_dot(Mat dest, Mat a, Mat b);
+void mat_sum(Mat dest, Mat a, Mat b);
+void mat_print(Mat m);
+void mat_rand(Mat m, float low, float high);
 
 #endif //NN_H_
 
 #ifndef NN_IMPLEMENTATION
+
+float rand_float(void) {
+	(float)rand() / (float)RAND_MAX; //Random number from 0 to 1
+}
+
+Mat mat_alloc(size_t rows, size_t cols) {
+	Mat m;
+	m.rows = rows;
+	m.cols = cols;
+	m.es = NN_malloc(sizeof(*m.es));
+	NN_assert(m.es != NULL);
+	return m;
+
+};
+void mat_dot(Mat dest, Mat a, Mat b) {
+	(void) dest;
+	(void) a;
+	(void) b;
+}
+void mat_sum(Mat dest, Mat a, Mat b) {
+	(void)dest;
+	(void)a;
+	(void)b;
+}
+
+void mat_print(Mat m) {
+	for (size_t i = 0; i < m.rows; i++) {
+		for (size_t j = 0; j < m.cols; j++) {
+			printf("%f ", MAT_AT(m, i, j));
+		}
+		printf("\n");
+	}
+}
+
+void mat_rand(Mat m, float low, float high) {
+	for (size_t i = 0; i < m.rows; i++) {
+		for (size_t j = 0; j < m.cols; j++) {
+			MAT_AT(m, i, j) = rand_float() * (high - low) + low;
+		}
+	}
+}
+
+
 
 #endif //NN_IMPLEMENTATION
